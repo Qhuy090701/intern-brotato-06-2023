3a95d5cb92eee21b1937b14f388aa9a23ddeb2de
GearIncQhuy <tqhuy9701@gmail.com>
2024-01-18 15:51:50 +0700

    HuyTQ_Create setup position magic

--- Assets/_Game/Scripts/Weapons/WeaponController.cs
+++ Assets/_Game/Scripts/Weapons/WeaponController.cs
@@ -5,6 +5,36 @@ using UnityEngine;
 
 public class WeaponController : MonoBehaviour {
   public Transform player;
+  [SerializeField] private Transform[] weaponPoint;
+
+  private void Awake() {
+    foreach (Transform weapon in weaponPoint) {
+      if (weaponPoint[0]) {
+        weaponPoint[0].position = new Vector3(player.transform.position.x, player.transform.position.y + 3f, 0f);
+      } 
+      
+      if (weaponPoint[1]) {
+        weaponPoint[1].position = new Vector3(-3, player.transform.position.y + 1.5f, 0f);
+      }
+      
+      if (weaponPoint[2]) {
+        weaponPoint[2].position = new Vector3(3, player.transform.position.y + 1.5f, 0f);
+      }
+      
+      if (weaponPoint[3]) {
+        weaponPoint[3].position = new Vector3(-3, player.transform.position.y -1.5f, 0f);
+      }
+      
+      if (weaponPoint[4]) {
+        weaponPoint[4].position = new Vector3(3, player.transform.position.y -1.5f, 0f);
+      }
+      
+      if (weaponPoint[5]) {
+        weaponPoint[5].position = new Vector3(player.transform.position.x, player.transform.position.y - 3f, 0f);
+      }
+    }
+  }
+
   private void Update() {
     FollowPlayer();
   }
